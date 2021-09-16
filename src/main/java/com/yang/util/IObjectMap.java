6c333b45fc7f2eed7b2e1b3914ad89590c4c804a
ChinaSnake007 <1505828047@qq.com>
2021-09-16 14:39:12 +0800

    新建分支

--- src/main/java/com/yang/util/IObjectMap.java
+++
@@ -1,8 +0,0 @@
-package com.yang.util;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface IObjectMap<T>{
-    public T getInstaceByResultSet(ResultSet rs) throws SQLException;
-}
